60c5fbe106861d4e9509c2c2ec20d5d785e2511d
Lyncon Estevan Bernardo Baez <lyncon.baez@pti.org.br>
2025-12-30 10:53:03 -0300

    Client and server working

--- shared/build/entities/MyRoomState.d.ts
+++
@@ -1,5 +0,0 @@
-import { Schema } from "@colyseus/schema";
-export declare class MyRoomState extends Schema {
-    mySynchronizedProperty: string;
-}
-//# sourceMappingURL=MyRoomState.d.ts.map
--- shared/build/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './entities/MyRoomState';
-//# sourceMappingURL=index.d.ts.map
